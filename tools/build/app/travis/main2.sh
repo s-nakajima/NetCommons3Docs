a639c4e63b71a79733da650f02908b3e30a77d0b
s-nakajima <nakajimashouhei@gmail.com>
2016-04-23 02:20:19 +0900

    自動でPHPDocを作成するように修正

--- tools/build/app/travis/main2.sh
+++ tools/build/app/travis/main2.sh
@@ -28,7 +28,7 @@ do
     rm -r phpdoc/$plugin
   fi
   echo "phpdoc ${APP_ROOT}/Plugin/$plugin"
-  phpdoc run -d "$PHPDOC_OPTIONS,${APP_ROOT}/Plugin/$plugin" -t phpdoc/$plugin --force --ansi > tee $LOG
+  phpdoc run -d "$PHPDOC_OPTIONS,${APP_ROOT}/Plugin/$plugin" -t phpdoc/$plugin --force --ansi > $LOG
 
 #  if [ `grep -c '\[37;41m' $LOG` -ne 0 ]; then
 #    cat $LOG
