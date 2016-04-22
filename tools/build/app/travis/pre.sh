eb833c59d0c7c290bb263ccd80f3966eb7750ae8
s-nakajima <nakajimashouhei@gmail.com>
2016-04-22 18:02:36 +0900

    自動でPHPDocを作成するように修正

--- tools/build/app/travis/pre.sh
+++ tools/build/app/travis/pre.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -ex
 
+WORKSPACE=$TRAVIS_BUILD_DIR
+
 sudo apt-get install graphviz
 sudo pip install sphinx
 mkdir -p build/logs
 composer global require phpdocumentor/phpdocumentor:2.*
 phpenv rehash
 set +H
-export PATH=$PATH:./vendors/bin
 
-WORKSPACE=$TRAVIS_BUILD_DIR
+export PATH=$PATH:./vendors/bin
 
 git clone -b develop git://github.com/$NETCOMMONS3DOCS $NETCOMMONS_BUILD_DIR
 cd $NETCOMMONS_BUILD_DIR
